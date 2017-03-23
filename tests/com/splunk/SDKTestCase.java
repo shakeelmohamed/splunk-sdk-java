78a279e8c98f299f548b76ab8e8390b8ed15b738
Shakeel Mohamed <smohamed@splunk.com>
2017-03-22 18:06:27 -0700

    trying something very extreme

--- tests/com/splunk/SDKTestCase.java
+++ tests/com/splunk/SDKTestCase.java
@@ -23,13 +23,17 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.security.Permission;
+import java.security.PermissionCollection;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -99,6 +103,53 @@ public abstract class SDKTestCase {
         return Integer.parseInt(ver.substring(2, 3));
     }
 
+    private static void removeCryptographyRestrictions() {
+        if (!isRestrictedCryptography()) {
+            System.out.println("Cryptography restrictions removal not needed");
+            return;
+        }
+        try {
+            /*
+             * Do the following, but with reflection to bypass access checks:
+             *
+             * JceSecurity.isRestricted = false;
+             * JceSecurity.defaultPolicy.perms.clear();
+             * JceSecurity.defaultPolicy.add(CryptoAllPermission.INSTANCE);
+             */
+            final Class<?> jceSecurity = Class.forName("javax.crypto.JceSecurity");
+            final Class<?> cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions");
+            final Class<?> cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission");
+
+            final Field isRestrictedField = jceSecurity.getDeclaredField("isRestricted");
+            isRestrictedField.setAccessible(true);
+            final Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(isRestrictedField, isRestrictedField.getModifiers() & ~Modifier.FINAL);
+            isRestrictedField.set(null, false);
+
+            final Field defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy");
+            defaultPolicyField.setAccessible(true);
+            final PermissionCollection defaultPolicy = (PermissionCollection) defaultPolicyField.get(null);
+
+            final Field perms = cryptoPermissions.getDeclaredField("perms");
+            perms.setAccessible(true);
+            ((Map<?, ?>) perms.get(defaultPolicy)).clear();
+
+            final Field instance = cryptoAllPermission.getDeclaredField("INSTANCE");
+            instance.setAccessible(true);
+            defaultPolicy.add((Permission) instance.get(null));
+
+            System.out.println("Successfully removed cryptography restrictions");
+        } catch (final Exception e) {
+            System.out.println("Failed to remove cryptography restrictions" + e.getMessage());
+        }
+    }
+
+    private static boolean isRestrictedCryptography() {
+        // This simply matches the Oracle JRE, but not OpenJDK.
+        return "Java(TM) SE Runtime Environment".equals(System.getProperty("java.runtime.name"));
+    }
+
     @Before
     public void setUp() throws Exception {
         // If using Charles Proxy for debugging, uncomment these lines.
@@ -126,12 +177,9 @@ public abstract class SDKTestCase {
         SSLSocketFactory TLSOnlySSLFactory = sslContext.getSocketFactory();
         Service.setSSLSocketFactory(TLSOnlySSLFactory);
 
-        try {
-            Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
-            field.setAccessible(true);
-            field.set(null, java.lang.Boolean.FALSE);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        // Weaken 
+        if (System.getenv("TRAVIS") != null) {
+            removeCryptographyRestrictions();
         }
 
         command = Command.splunk();
